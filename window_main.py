2c51c1bec60267885bed72c6a2c3d91311dd8ef2
liboyang <lby920101@gmail.com>
2018-11-08 03:17:51 +0800

    Disable file browser button while selecting files

--- window_main.py
+++ window_main.py
@@ -17,8 +17,8 @@ def create_main_windows():
             if each[0].Key == 'PLACE_HOLDER':
                 window.Rows.remove(each)
                 break
-        if len(window.Rows) < 20:
-            place_holder = sg.Text('', key='PLACE_HOLDER', size=(0, 20 - len(window.Rows)))
+        if len(window.Rows) < 10:
+            place_holder = sg.Text('', key='PLACE_HOLDER', size=(0, 10 - len(window.Rows)))
             window.Rows.append([place_holder])
         _window_show(non_blocking)
     window.Show = _new_window_show
@@ -27,13 +27,14 @@ def create_main_windows():
     menu_layout = [
         ['File', ['Config', 'Exit']]
     ]
-    menu = sg.Menu(menu_layout)
+    menu = sg.Menu(menu_layout, background_color='#FFFFFF')
     # Row 1: Post
     files_selector = sg.FilesBrowse('添加文件', key='ADD_FILES', target=(None, None), size=(12, 1))
     _selector_callback = files_selector.ButtonCallBack
 
     # 拦截按钮回调
     def _new_selector_callback():
+        files_selector.Update(disabled=True)
         _selector_callback()
         files = files_selector.TKStringVar.get().split(';')
         for each in files:
@@ -52,7 +53,7 @@ def create_main_windows():
         if any(['REMOVE_' + filename == each[3].Key for each in window.Rows[3:] if len(each) > 2]):
             return
         # 创建删除文件按钮
-        remove_button = sg.Button('删除', change_submits=True, size=(5, 1))
+        remove_button = sg.Button('删除', change_submits=True, size=(5, 1), button_color=('#FFFFFF', '#FF0000'))
         remove_button.Key = 'REMOVE_' + filename
         _button_callback = remove_button.ButtonCallBack
 
@@ -65,12 +66,13 @@ def create_main_windows():
                     break
         remove_button.ButtonCallBack = _new_button_callback
         # 开始/暂停/恢复按钮
-        start_pause_resume_button = sg.Button('开始', change_submits=False, size=(5, 1))
+        start_pause_resume_button = sg.Button('开始', change_submits=False,
+                                              size=(5, 1), button_color=('#FFFFFF', '#66CC66'))
         start_pause_resume_button.Key = 'START_' + filename
         # 添加新行
         window.Rows.append([
             sg.Text('上传 ' + filename.split('/')[-1], size=(20, 1), background_color='#FFFFFF'
-                    ), sg.ProgressBar(100, size=(25, 1)), start_pause_resume_button, remove_button
+                    ), sg.ProgressBar(100, size=(25, 18)), start_pause_resume_button, remove_button
         ])
 
     # 设置布局
